50696298af4b81dbb55847398f49c8d40d2c6edf
highjeans <vivan.waghela23@gmail.com>
2026-08-21 10:49:27 +0100

    Add Visual Studio License accepted config

--- default.nix
+++ default.nix
@@ -2,7 +2,9 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {
     overlays = [ ];
-    config = { };
+    config = {
+      microsoftVisualStudioLicenseAccepted = true;
+    };
   };
 in
 {
