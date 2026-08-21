bf5d13fc12fcf8fa42beaea650d886889802ed54
highjeans <vivan.waghela23@gmail.com>
2026-08-21 13:10:02 +0100

    Remove windows builds

--- default.nix
+++ default.nix
@@ -3,11 +3,7 @@ let
   pkgs = import sources.nixpkgs {
     overlays = [ ];
     config = {
-      microsoftVisualStudioLicenseAccepted = true;
     };
   };
 in
-{
-  default = pkgs.callPackage ./package.nix { };
-  windows = pkgs.pkgsCross.x86_64-windows.callPackage ./package.nix { };
-}
+pkgs.callPackage ./package.nix { }
